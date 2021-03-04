1b9b2a09895f0cf4faa516687f6ded48046f3e2e
art <partypeopleland@gmail.com>
2021-03-04 15:45:21 +0800

    調整Demo的網頁，可自行輸入人名加入Chat

--- WS.Demo/Program.cs
+++ WS.Demo/Program.cs
@@ -16,17 +16,18 @@ namespace WS.Demo
             for (int i = 0; i < 1000; i++)
             {
                 wssv.AddWebSocketService<Echo>($"/Echo-{i}");
+                
             }
             
             wssv.Start();
-            if (wssv.IsListening)
-            {
-                Console.WriteLine("Listening on port {0}, and providing WebSocket services:", wssv.Port);
-                foreach (var path in wssv.WebSocketServices.Paths)
-                {
-                    Console.WriteLine("- {0}", path);
-                }
-            }
+            // if (wssv.IsListening)
+            // {
+            //     Console.WriteLine("Listening on port {0}, and providing WebSocket services:", wssv.Port);
+            //     foreach (var path in wssv.WebSocketServices.Paths)
+            //     {
+            //         Console.WriteLine("- {0}", path);
+            //     }
+            // }
 
             Console.WriteLine("\nPress Enter key to stop the server...");
             Console.ReadLine();
